7fab09328319708a1c41584b1ee94a8129d8fac7
JoseXP7 <josemanuelgraterolrodriguez@gmail.com>
2026-04-28 14:34:01 -0400

    feat: Tailwind configuration and created navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
-<script setup></script>
+<script setup>
+import NavBar from './components/layout/NavBar.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
+  <NavBar />
+  <h1 class="text-brand-primary">You did it!</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
