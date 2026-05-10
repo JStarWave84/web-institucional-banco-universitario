1649a873682e8c735430efae56dc9ed440719abf
JStarWave84 <josemanuelgraterolrodriguez@gmail.com>
2026-05-09 20:31:31 -0400

    feat: Added many sections to home view and new colors and font on main.css

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <script setup>
-import { CirclePlay, ShieldCheck } from '@lucide/vue'
+import {
+  CirclePlay,
+  ShieldCheck,
+  ArrowRightLeft,
+  Landmark,
+  Banknote,
+  CircleCheck,
+} from '@lucide/vue'
 </script>
 
 <template>
-  <section class="pt-20 pb-32">
+  <!--Hero section-->
+  <section class="pt-20 pb-32 bg-brand-bg">
     <div class="flex items-center gap-12 px-6">
       <div class="flex flex-col gap-6">
         <span
@@ -57,6 +65,131 @@ import { CirclePlay, ShieldCheck } from '@lucide/vue'
             </p>
           </div>
         </div>
+      </div>
+    </div>
+  </section>
+  <!--Services section-->
+  <section class="py-24 bg-brand-bg-secondary">
+    <div class="flex flex-col gap-16 px-6">
+      <div class="flex flex-col gap-4">
+        <h2 class="font-extrabold text-brand-primary text-[36px] leading-10">
+          Servicios Especializados
+        </h2>
+        <span class="block w-24 h-1.5 bg-brand-primary rounded-4xl"></span>
+      </div>
+
+      <div class="grid grid-cols-1 md:grid-cols-3 justify-items-center gap-8">
+        <div class="flex flex-col gap-4 p-8 bg-white shadow-lg rounded-4xl">
+          <div
+            class="bg-brand-bg-secondary rounded-4xl p-2 w-14 h-14 flex items-center justify-center"
+          >
+            <ArrowRightLeft class="text-brand-primary" />
+          </div>
+          <h3 class="pt-2 font-bold text-[20px] leading-7 text-brand-primary">
+            Transferencias sin comisión
+          </h3>
+          <p class="text-page-text text-[16px] leading-6.5">
+            Envía y recibe dinero entre compañeros de cualquier universidad del país al instante y
+            con costo cero.
+          </p>
+        </div>
+
+        <div class="flex flex-col gap-4 p-8 bg-white shadow-lg rounded-4xl">
+          <div
+            class="bg-brand-bg-secondary rounded-4xl p-2 w-14 h-14 flex items-center justify-center"
+          >
+            <Landmark class="text-brand-primary" />
+          </div>
+          <h3 class="pt-2 font-bold text-[20px] leading-7 text-brand-primary">Pago de matrícula</h3>
+          <p class="text-page-text text-[16px] leading-6.5">
+            Gestiona el pago de tus unidades de crédito y semestres directamente desde nuestra app
+            con convenios directos.
+          </p>
+        </div>
+
+        <div class="flex flex-col gap-4 p-8 bg-white shadow-lg rounded-4xl">
+          <div
+            class="bg-brand-bg-secondary rounded-4xl p-2 w-14 h-14 flex items-center justify-center"
+          >
+            <Banknote class="text-brand-primary" />
+          </div>
+          <h3 class="pt-2 font-bold text-[20px] leading-7 text-brand-primary">Cobro de becas</h3>
+          <p class="text-page-text text-[16px] leading-6.5">
+            Recibe tus depósitos de becas nacionales e institucionales de forma automática y segura
+            cada mes.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!--Education section-->
+  <section class="px-6 py-24 bg-brand-bg">
+    <div class="flex gap-16">
+      <div class="hidden lg:block w-1/2">
+        <img
+          src="../assets/img/education-image.jpg"
+          alt="Imagen de estudiantes universitarios aprendiendo sobre finanzas"
+          class="rounded-[48px] shadow-lg object-cover"
+        />
+      </div>
+
+      <div class="flex flex-col w-1/2 justify-center gap-4">
+        <span class="font-bold text-[14px] leading-5 tracking-[1.4px] text-brand-primary uppercase"
+          >Formación para el Futuro</span
+        >
+        <h2 class="font-extrabold text-[36px] leading-11.25">
+          Educación Financiera para Universitarios
+        </h2>
+        <p class="text-[18px] leading-7 pt-[7.25px] text-page-text">
+          No solo guardamos tu dinero, te enseñamos a multiplicarlo. Accede a talleres exclusivos
+          diseñados para la realidad económica del estudiante venezolano.
+        </p>
+
+        <ul class="pt-4 pb-6 space-y-4">
+          <li class="flex items-start gap-3">
+            <CircleCheck class="text-brand-primary" />
+            <span class="text-page-text text-[16px] leading-6.5">
+              Talleres prácticos sobre gestión financiera
+            </span>
+          </li>
+          <li class="flex items-start gap-3">
+            <CircleCheck class="text-brand-primary" />
+            <span class="text-page-text text-[16px] leading-6.5">
+              Seminarios sobre inversión y ahorro
+            </span>
+          </li>
+          <li class="flex items-start gap-3">
+            <CircleCheck class="text-brand-primary" />
+            <span class="text-page-text text-[16px] leading-6.5">
+              Asesoría personalizada para planificación financiera
+            </span>
+          </li>
+        </ul>
+        <div>
+          <a
+            href="#"
+            class="text-brand-primary font-bold text-[16px] leading-6 px-8 py-3 bg-teal-200 rounded-4xl hover:bg-brand-primary hover:text-teal-200 transition-colors"
+            >Explorar talleres</a
+          >
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Social media CTA-->
+  <section class="py-12 bg-teal-100/50">
+    <div class="px-6 flex justify-between">
+      <div>
+        <h2 class="font-bold text-[24px] leading-8 text-brand-primary">
+          Únete a nuestras redes sociales
+        </h2>
+        <p class="text-page-text text-[16px] leading-6">
+          Entérate de sorteos, eventos y nuevos beneficios cada semana.
+        </p>
+      </div>
+      <div>
+        <div></div>
       </div>
     </div>
   </section>
