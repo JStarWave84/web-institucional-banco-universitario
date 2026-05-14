c26ca7d66f524bc06ca14e42c88462849658ac99
Pedro Rivero <pmrc980@gmail.com>
2026-05-14 14:24:50 -0400

    feat: add first hero section of financial education view

--- src/views/FinancialEducationView.vue
+++ src/views/FinancialEducationView.vue
@@ -3,23 +3,58 @@ import {} from '@lucide/vue'
 </script>
 
 <template>
-  <main>
-    <section class="pt-20 pb-32 bg-brand-bg">
-      <div class="px-6 flex flex-col gap-8 text-center items-center">
+  <section class="pt-20 pb-32 bg-brand-bg">
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-12 px-6">
+      <div class="flex flex-col gap-6">
         <span
           class="text-brand-primary bg-teal-300/30 px-4 py-2 uppercase text-[14px] font-bold rounded-4xl w-max"
+          >Oasis Académico</span
         >
-          Oasis Académico
-        </span>
-        <h1 class="font-extrabold text-[42px] md:text-[64px] leading-tight">
-          TuFuturo, <span class="text-[#00928b]">Financieramente</span> <br />
-          brillante.
+        <h1 class="font-extrabold text-[42px] md:text-[72px] leading-11 md:leading-18">
+          Tu futuro <br />
+          <span class="text-[#00928b]">Financieramente</span> <br />
+          Brillante.
         </h1>
-        <p class="text-[18px] md:text-[20px] max-w-2xl text-page-text">
-          No solamente guardamos tu dinero, cultivamos tu conocimiento. Domina el arte del ahorro,
-          la inversión y el crédito responsable con programas diseñados para la vida universitaria.
-        </p>
+
+        <div class="pt-2 w-full md:w-lg">
+          <p class="text-[18px] md:text-[18px] leading-7 text-page-text">
+            No solo guardamos tu dinero, cultivamos tu conocimiento. Domina el arte del ahorro, la
+            inversión y el crédito responsable con programas diseñados para la vida universitaria.
+          </p>
+        </div>
+
+        <div class="flex flex-col gap-4 pt-4 sm:flex-row sm:items-center">
+          <a
+            href="#"
+            class="bg-brand-primary text-white font-bold leading-7 px-8 py-4 rounded-4xl shadow-lg hover:bg-brand-secondary transition-colors w-full sm:w-auto text-center"
+          >
+            Inscribirse a Talleres
+          </a>
+          <a
+            href="#"
+            class="bg-gray-200 text-brand-primary font-bold leading-7 px-8 py-4 flex items-center justify-center gap-2 rounded-4xl hover:bg-brand-primary hover:text-white transition-colors w-full sm:w-auto"
+          >
+            <CirclePlay /> Explorar Recursos
+          </a>
+        </div>
       </div>
-    </section>
-  </main>
+      <div class="relative group">
+        <img
+          src="../assets/img/fe-1.png"
+          alt="Ilustración de estudiantes"
+          class="w-full rounded-[48px] shadow-lg object-cover relative z-10"
+        />
+        <div
+          class="absolute -bottom-6 left-1/2 -translate-x-1/2 lg:left-0 lg:-translate-x-6 lg:bottom-12 z-20 bg-white rounded-4xl shadow-xl px-6 py-6 flex gap-4 w-72 md:w-81"
+        >
+          <div>
+            <h3 class="text-[14px] leading-5 text-brand-primary font-bold">+2,500 Estudiantes</h3>
+            <p class="text-page-text text-[12px] leading-4">
+              Capacitados en educación financiera este semestre.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
