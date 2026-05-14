a289f0253f06b67384af67f4863cb5878626bc67
Marlon <1001.28712802.ucla@gmail.com>
2026-05-14 17:51:13 -0400

    new-view-alianza

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,11 @@ const router = createRouter({
       component: () => import('../views/HomeView.vue'),
     },
     {
+      path: '/alianzas',
+      name: 'Alianzas',
+      component: () => import('../views/AlianzasView.vue'),
+    },
+    {
       path: '/sobre-nosotros',
       name: 'SobreNosotros',
       component: () => import('../views/SobreNosotrosView.vue'),
