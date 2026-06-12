83fc08e4b675fec683c2cb9061700dfa3d586182
DanielRuiz3101 <darp0131@gmail.com>
2026-06-11 23:49:07 -0400

    Add More Details in Contact_BankView

--- src/views/OnlineBanking/Contact_BankViews.vue
+++ src/views/OnlineBanking/Contact_BankViews.vue
@@ -68,13 +68,77 @@ import { Plus, Search, LayoutGrid, ListFilter, ArrowRight, UserPlus, Pen, Trash 
 
       <!--Contact Card3-->
       <div
-        class="w-[298.67px] h-72 rounded-4xl pt-6 px-6 pb-7 bg-white shadow-[0px_1px_2px_0px_#0000000D] row-start-1 col-start-3 col-span-1 row-span-1"
-      ></div>
+        class="w-[298.67px] h-72 pt-6 px-6 pb-7 rounded-4xl bg-[white] border-2 border-[#A8EFF3] shadow-[0px_1px_2px_0px_#0000000D] row-start-1 col-start-3 col-span-1 row-span-1"
+      >
+        <div class="w-[250.67px] h-22 mb-2">
+          <div class="flex justify-between h-15.5">
+            <div
+              class="w-16 h-16 rounded-2xl pt-[15.5px] pb-[16.5px] bg-[#85F5ED] text-center text-[#00201E] font-bold text-[24px]"
+            >
+              CR
+            </div>
+
+            <div
+              class="flex items-center justify-center w-[68.47px] h-5.75 px-2 py-1 gap-1 rounded-md bg-[#A8EFF3]"
+            >
+              <span class="font-black text-[10px] text-[#002021]">FRECUENTE</span>
+            </div>
+          </div>
+        </div>
+
+        <div class="w-[250.67px]">
+          <div class="flex flex-col h-18 mb-7">
+            <h3 class="h-7 font-bold text-[20px] text-[#191C1D]">Carlos Ruiz</h3>
+            <span class="h-5 text-[14px] text-[#3F4949]">**** 1102</span>
+            <span class="h-6 mt-1 text-[14px] text-[#6F7979]">Cena grupo estudio</span>
+          </div>
+        </div>
+
+        <button
+          class="w-[250.67px] h-11 py-3 rounded-xl bg-linear-to-r from-[#004649] to-[#085F63] font-bold text-[14px] text-white"
+        >
+          Enviar Dinero
+        </button>
+      </div>
 
       <!--Contact Card2-->
       <div
         class="w-[298.67px] h-72 rounded-4xl pt-6 px-6 pb-7 bg-white shadow-[0px_1px_2px_0px_#0000000D] row-start-1 col-start-2 col-span-1 row-span-1"
-      ></div>
+      >
+        <div class="w-[250.67px] h-22 mb-2">
+          <div class="flex justify-between h-15.5">
+            <div
+              class="w-16 h-16 rounded-2xl pt-[15.5px] pb-[16.5px] bg-[#85F5ED] text-center text-[#00201E] font-bold text-[24px]"
+            >
+              VP
+            </div>
+
+            <div class="w-17.5 h-8.5 gap-1">
+              <button class="w-8.5 h-8.5 rounded-full p-2">
+                <Pen class="w-4.5 h-4.5 text-[#6F7979]" />
+              </button>
+
+              <button class="w-8 h-8.5 rounded-full p-2">
+                <trash class="w-4.5 h-4.5 text-[#BA1A1A]" />
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div class="w-[250.67px]">
+          <div class="flex flex-col h-18 mb-7">
+            <h3 class="h-7 font-bold text-[20px] text-[#191C1D]">Valeria Peña</h3>
+            <span class="h-5 text-[14px] text-[#3F4949]">**** 4490</span>
+            <span class="h-6 mt-1 text-[14px] text-[#6F7979]">Materiales arquitectura</span>
+          </div>
+        </div>
+
+        <button
+          class="w-[250.67px] h-11 py-3 rounded-xl bg-[#E7E8E9] font-bold text-[14px] text-[#004649]"
+        >
+          Enviar Dinero
+        </button>
+      </div>
 
       <!--Contact Card1-->
       <div
