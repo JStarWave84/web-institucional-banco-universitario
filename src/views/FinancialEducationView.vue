221c82c9bf449c3f39a36b3e48885ebaa8e857e9
Pedro Rivero <pmrc980@gmail.com>
2026-07-19 13:30:12 -0400

    fix(FinancialEducationView): apply corrections

--- src/views/FinancialEducationView.vue
+++ src/views/FinancialEducationView.vue
@@ -12,7 +12,7 @@ import {
 
 <template>
   <!--(Sección 1) Hero-->
-  <section class="pt-20 pb-32 bg-brand-bg">
+  <section class="pt-20 pb-16 bg-brand-bg">
     <div class="grid grid-cols-1 lg:grid-cols-2 gap-12 px-6">
       <div class="flex flex-col gap-6">
         <span
